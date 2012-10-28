1317b8fea284820799ea413130a489d3d7613f29
Piyanan Kamlangmark_home <Duck_ComEn@hotmail.com>
2012-10-28 16:02:10 +0700

    reset

--- theme/fadeback/version.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || die;
-
-$plugin->version   = 2012080901;
-$plugin->component = 'theme_fadeback';
-$plugin->requires  = 2011120500;
-$plugin->maturity  = MATURITY_STABLE;
