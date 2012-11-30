bdec92dfed24b9cc62fd432881e882874b6bbd98
Piyanan <Duck_ComEn@hotmail.com>
2012-11-30 12:48:49 +0700

    reset brance database & new upload

--- ros/Data/examples/basic_init/Connections/file.php
+++
@@ -1,5 +0,0 @@
-<?php
-mysql_connect('localhost','root','');
-mysql_select_db("moodle");
- mysql_query("SET NAMES UTF8");
-?>
--- ros/logout.php
+++
@@ -1,4 +0,0 @@
-﻿<?php  	// กรณีที่คลิกเมนู "ออกจากระบบ" จะทำลายตัวแปร Session
-//@session_start(); 
-@session_destroy();
-echo "<meta http-equiv='refresh' content='0;URL=index.php'>";  ?>
